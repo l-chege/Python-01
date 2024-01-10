c8e9051904ddeeded0635094882f45f98ffed001
Laura Chege <laurachege9@gmail.com>
2024-01-10 11:11:02 +0300

    nested if...else loop update

--- main.py
+++ main.py
@@ -53,17 +53,18 @@ scope_test(35)
 
 #user input validation - if.....else conditional statement
 def days_to_units(num_of_days):
-    if num_of_days > 0:
-        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
-    elif num_of_days == 0:  
-        return "you entered a 0, please enter a valid positive number"
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+   
     
-#encapsulation
+#encapsulation     #nested loop (loop inside a loop)
 def validate_and_execute():
     if user_input.isdigit():                                                                #isdigit() is a built-in function to check if the input is a number
         user_input_number = int(user_input)
-        calculated_value = days_to_units(user_input_number)
-        print(calculated_value)
+        if user_input_number > 0:       
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a 0, please enter a valid positive number")
     else:                                                                                  #if the input is not a number, the program will print the following message
         print("your input is not a valid number. Don't ruin my program!")
 
