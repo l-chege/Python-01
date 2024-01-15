5f8be299eed4b6516a0df86b5fb642f1eda21b0b
Laura Chege <lauracahege9@gmail.com>
2024-01-15 09:43:01 +0300

    dictionary update

--- main.py
+++ main.py
@@ -121,7 +121,21 @@ my_set.remove("January")                                 #removing elements from
 print(my_set)"""
 
 
+#dictionary
+user_input = ""
+while user_input != "exit":
+    user_input = input("Hey user, enter number of days and conversion unit! \n")
+    days_and_unit = user_input.split(":")
+    print(days_to_units)
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}   #dictionary syntax
+    print(days_and_unit_dictionary)
+    validate_and_execute()
 
+my_list = ["20", "30", "40"]
+print(my_list[2])
+
+my_dictionary = {"days":20, "unit": "hrs", "message": "Awesome!"}
+print(my_dictionary["message"])
 
 
 
