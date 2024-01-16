1d275ca554fd608883adca9d5a982d9d9388cdd0
Laura Chege <laurachege9@gmail.com>
2024-01-16 03:45:46 +0300

    minor fixes

---
+++ 30DaysSolutions/Day4_Strings.py
@@ -0,0 +1,16 @@
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+
+
+
+
+
+
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+#Declare a variable named company and assign it to an initial value "Coding For All".
+#Print the variable company using print().
+#Print the length of the company string using len() method and print().
+#Change all the characters to uppercase letters using upper() method.
+#Change all the characters to lowercase letters using lower() method.
+#Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+#Cut(slice) out the first word of Coding For All string.
+#Check if Coding For All string contains a word Coding using the method index, find or other methods.
