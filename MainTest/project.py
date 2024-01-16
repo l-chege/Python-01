ea5bf1b644a1dffc35786c8d49671fac07d777f3
Laura Chege <laurachege9@gmail.com>
2024-01-16 09:41:33 +0300

    minor updates

--- MainTest/project.py
+++ MainTest/project.py
@@ -1,14 +1,14 @@
 import  openpyxl      
 
-inv_file = openpyxl.load_workbook("inventory.xlsx")  
-product_list = inv_file["Sheet1"]
+inv_file = openpyxl.load_workbook("inventory.xlsx")  #load workbook
+product_list = inv_file["Sheet1"]               
 
-products_per_supplier = {}  
+products_per_supplier = {}   
 total_value_per_supplier = {} 
 products_under_10_inv = {}    
 
 for product_row in range(2, product_list.max_row + 1):              
-    supplier_name = product_list.cell(product_row, 4).value   
+    supplier_name = product_list.cell(product_row, 4).value        
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 3).value
     product_num = product_list.cell(product_row, 1).value
